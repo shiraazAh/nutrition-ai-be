ef4de8fcf57ca873695c72eea7221bfed37178f4
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2025-03-15 15:03:39 +0000

    AWS Lamda deploy test #2

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!- Updated V2- check'),
+        body: JSON.stringify('Hello from Lambda!- Updated V2'),
     };
     return response;
 };
